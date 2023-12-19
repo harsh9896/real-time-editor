9c4c7dfb7333d2fe0d18bda1d405e246dc17b9ac
harsh9896 <hgoyal044@gmail.com>
2023-12-20 00:08:18 +0530

    Home Page

---
+++ editor_ui/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Editorpages from './pages/Editorpages';
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/editor/:roomId' element={<Editorpages />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default App;
---
+++ editor_ui/src/pages/Editorpages.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Editorpages = () => {
+  return (
+    <div>editor pages</div>
+  )
+}
+
+export default Editorpages
